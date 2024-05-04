27f96f42195679019fc57ce6d978a53ccce84b33
LIan2nd <alfiannurusyaid19@gmail.com>
2024-05-04 20:37:37 +0700

    init setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./pages/Home/Home";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h2>Covid App</h2>
+      <h3>Final Project Frontend Engineer</h3>
+    </div>
+  )
+}
+
+export default Navbar;
